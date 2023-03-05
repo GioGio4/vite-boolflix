23393442202069de3cfbae9aebcbdbd2bffa43b2
GioGio4 <g.ceccobelli@outlook.it>
2023-03-05 17:01:34 +0100

    Create AppHeader form,emits =>in APP. App call axios with data (forms)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <script>
 import AppMain from "./components/AppMain.vue";
 import AppHeader from "./components/AppHeader.vue";
+import axios from "axios";
+import { store } from "./Data/Store";
 
 export default {
+  data() {
+    return {
+      store,
+    }
+  },
 
-  components: { AppMain, AppHeader }
+  components: { AppMain, AppHeader },
+
+  methods: {
+    // Al click o al rilascio del tasto invio vengono svuotate le liste nello store, poi viene 
+    // richiamato axios aggiungendo il "tearm" dopo la query= (API)
+    search(tearm) {
+      store.filmSearchedList = [],
+        store.seriesSearchedList = [],
+
+        // Lista film cercati
+        axios.get(("https://api.themoviedb.org/3/search/movie?api_key=3ad2b809de5ceef6ec6bd710fdbb1aab&language=it-IT&query=") + (tearm))
+          .then((response) => {
+            store.filmSearchedList = response.data.results
+          })
+
+      // Lista serie tv cercate
+      axios.get(("https://api.themoviedb.org/3/search/tv?api_key=3ad2b809de5ceef6ec6bd710fdbb1aab&language=it-IT&query=") + (tearm))
+        .then((response) => {
+          store.seriesSearchedList = response.data.results
+        })
+
+    }
+
+  },
 }
 
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader @on-search="search" />
+
+  <!-- Temporaneo stampa informazioni film ricevuti  IMPORTANTE: ricordare il v-if altrimenti appena aperta la app da errore
+    dato che la lista è ancora vuota  -->
+  <div v-if="store.filmSearchedList != ''" v-for="film in store.filmSearchedList">
+    <h1>{{ film.title }}</h1>
+    <p>{{ film.overview }}</p>
+  </div>
+
   <AppMain />
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,37 @@
 <script>
 
 export default {
+    data() {
+        return {
+            userSearch: "",
+        };
+    },
+
+    emits: ["on-search"],
+
+    // Al click sul bottone "invia" viene passato quello che è inserito nel "userSearch" al genitore (in questo caso una stringa
+    // che contiene la ricerca dell'utente)
+    methods: {
+        search() {
+            this.$emit("on-search", this.userSearch)
+        }
+    }
 
 }
 
 </script>
 
-<template></template>
+<template>
+    <!-- Template provvisorio - barra di ricerca + bottone invio dati -->
+
+    <form @submit.prevent="">
+        <div class="input-group mb-3">
+            <input v-model="userSearch" type="text" class="form-control" placeholder="Cerca..." aria-label="Cerca..."
+                aria-describedby="button-addon2" @keyup.enter="search">
+            <button class="btn btn-outline-secondary" type="button" @click="search">Invia</button>
+        </div>
+
+    </form>
+</template>
 
 <style lang="scss"></style>
