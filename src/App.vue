2542ab72d42ee2594a190b51443c309ffb03d2e2
GioGio4 <g.ceccobelli@outlook.it>
2023-03-05 15:14:24 +0100

    Create components (header-main) + store js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script>
+import AppMain from "./components/AppMain.vue";
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+
+  components: { AppMain, AppHeader }
+}
 
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
 
 <style lang="scss"></style>
